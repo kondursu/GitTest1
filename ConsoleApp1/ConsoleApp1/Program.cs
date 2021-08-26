542d1fdc0f3f0c100dda7ccf0a21502281dbb0a4
kondursu <89560318+kondursu@users.noreply.github.com>
2021-08-25 21:55:55 -0500

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //code was added in Github
+            //code was edited in Github
         }
     }
 }
